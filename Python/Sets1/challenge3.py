0661f7d744a24198a2bf45124e2b60b1b578cc07
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2022-03-02 18:49:01 -0300

    Sets 1- Challenge 3 - Solved
    While I was meant to use character frequency to only output the most sensible one, I also wanted to see the other results.

--- Python/Sets1/challenge3.py
+++ Python/Sets1/challenge3.py
@@ -18,3 +18,25 @@ Evaluate each output and choose the one with the best score.
 
 "You now have our permission to make "ETAOIN SHRDLU" jokes on Twitter."
 """
+
+def convert_hex_to_raw_byte(input_str: str):
+    return bytes.fromhex(input_str)
+
+def string_to_raw_byte(input_str: str):    
+    return input_str.encode('utf-8')
+
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+hex_enc_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+raw_enc_str = convert_hex_to_raw_byte(hex_enc_str)
+
+for i in range(97, 123):
+    current_test = string_to_raw_byte(chr(i)*len(raw_enc_str))
+    test = byte_xor(raw_enc_str, current_test).decode("utf-8") 
+    print("Letter.: " + chr(i))
+    print(test)
+    print("-------")
+    
+after_run = string_to_raw_byte("x"*len(raw_enc_str))
+print(byte_xor(raw_enc_str, after_run).decode("utf-8"))
